8421d7331023a853426d6441ca4280b8f4923bb8
joseph-wee <starlightkor@gmail.com>
2022-12-02 11:54:16 +0900

    [TEST] - release test

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -35,11 +35,12 @@ const LoginPage = () => {
     const loginRequest = async () => {
       if(emailAvailable == 1 && pwAvailable == 1) {
       await axios
-        .post(`http://localhost:8080/member/login`, {
+        .post(`http://dailygram-env-2.eba-33ajdt9q.ap-northeast-2.elasticbeanstalk.com/member/login`, {
           email: emailValue,
           password: pwValue
             })
         .then(res => {
+          alert("통신성공")
           if(res.data.result == true) {
             setCookie("accessToken", res.data.accessToken, 1)
             setCookie("refreshToken", res.data.refreshToken, 1)
